5904d54ac38ef1854bf9cbe1c87f153ea0d6cff0
Sander Pick <sanderpick@gmail.com>
2024-09-01 21:01:13 -0700

    use eip1559 transactions
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- src/server/register.rs
+++ src/server/register.rs
@@ -1,16 +1,20 @@
+use anyhow::anyhow;
+use ethers::core::k256::ecdsa::SigningKey;
 use ethers::prelude::{
-    Address, Http, LocalWallet, Middleware, Provider, Signer, SignerMiddleware, TransactionRequest,
-    TxHash, U256,
+    Address, Eip1559TransactionRequest, Http, LocalWallet, Middleware, Provider, Signer,
+    SignerMiddleware, TransactionReceipt, Wallet, I256, U256,
 };
 use serde_json::json;
 use std::convert::TryFrom;
-use std::error::Error;
+use std::sync::Arc;
 use warp::{Filter, Rejection, Reply};
 
 use crate::server::{
     shared::{with_private_key, with_rpc_url, BadRequest, BaseRequest},
     util::log_request_body,
 };
+
+type DefaultSignerMiddleware = SignerMiddleware<Provider<Http>, Wallet<SigningKey>>;
 
 /// Route filter for `/register` endpoint.
 pub fn register_route(
@@ -47,7 +51,7 @@ pub async fn handle_register(
                 message: format!("register error: {}", e),
             })
         })?;
-    let json = json!(res);
+    let json = json!({"tx_hash": res.transaction_hash});
     Ok(warp::reply::json(&json))
 }
 
@@ -57,7 +61,7 @@ pub async fn send_zero(
     address: Address,
     private_key: String,
     rpc_url: String,
-) -> anyhow::Result<Option<TxHash>, Box<dyn Error>> {
+) -> anyhow::Result<TransactionReceipt> {
     let node_url = rpc_url;
     let provider = Provider::<Http>::try_from(node_url.to_string())?;
     let chain_id = provider.get_chainid().await?.as_u64();
@@ -65,12 +69,119 @@ pub async fn send_zero(
     // Parse the private key from hex string
     let private_key_bytes = hex::decode(private_key)?;
     let wallet = LocalWallet::from_bytes(&private_key_bytes)?.with_chain_id(chain_id);
-
     let client = SignerMiddleware::new(provider, wallet);
+    let client = Arc::new(client);
 
-    let tx = TransactionRequest::pay(address, U256::zero());
-    let receipt = client.send_transaction(tx, None).await?.await?;
+    let (fee, fee_cap) = premium_estimation(client.clone()).await?;
+    let tx = Eip1559TransactionRequest::new()
+        .to(address)
+        .value(U256::zero())
+        .max_priority_fee_per_gas(fee)
+        .max_fee_per_gas(fee_cap);
+    let tx_pending = client.send_transaction(tx, None).await?;
+    tx_pending
+        .await?
+        .ok_or(anyhow!("send did not return receipt"))
+}
 
-    let hash = receipt.map(|r| r.transaction_hash);
-    Ok(hash)
+/// Returns an estimation of an optimal `gas_premium` and `gas_fee_cap`
+/// for a transaction considering the average premium, base_fee and reward percentile from
+/// past blocks
+/// This is an adaptation of ethers' `eip1559_default_estimator`:
+/// https://github.com/gakonst/ethers-rs/blob/5dcd3b7e754174448f9a8cbfc0523896609629f9/ethers-core/src/utils/mod.rs#L476
+async fn premium_estimation(signer: Arc<DefaultSignerMiddleware>) -> anyhow::Result<(U256, U256)> {
+    let base_fee_per_gas = signer
+        .get_block(ethers::types::BlockNumber::Latest)
+        .await?
+        .ok_or_else(|| anyhow!("Latest block not found"))?
+        .base_fee_per_gas
+        .ok_or_else(|| anyhow!("EIP-1559 not activated"))?;
+
+    let fee_history = signer
+        .fee_history(
+            ethers::utils::EIP1559_FEE_ESTIMATION_PAST_BLOCKS,
+            ethers::types::BlockNumber::Latest,
+            &[ethers::utils::EIP1559_FEE_ESTIMATION_REWARD_PERCENTILE],
+        )
+        .await?;
+
+    let max_priority_fee_per_gas = estimate_priority_fee(fee_history.reward); //overestimate?
+    let potential_max_fee = base_fee_surged(base_fee_per_gas);
+    let max_fee_per_gas = if max_priority_fee_per_gas > potential_max_fee {
+        max_priority_fee_per_gas + potential_max_fee
+    } else {
+        potential_max_fee
+    };
+
+    Ok((max_priority_fee_per_gas, max_fee_per_gas))
+}
+
+/// Implementation borrowed from
+/// https://github.com/gakonst/ethers-rs/blob/ethers-v2.0.8/ethers-core/src/utils/mod.rs#L582
+/// Refer to the implementation for unit tests
+fn base_fee_surged(base_fee_per_gas: U256) -> U256 {
+    if base_fee_per_gas <= U256::from(40_000_000_000u64) {
+        base_fee_per_gas * 2
+    } else if base_fee_per_gas <= U256::from(100_000_000_000u64) {
+        base_fee_per_gas * 16 / 10
+    } else if base_fee_per_gas <= U256::from(200_000_000_000u64) {
+        base_fee_per_gas * 14 / 10
+    } else {
+        base_fee_per_gas * 12 / 10
+    }
+}
+
+/// Implementation borrowed from
+/// https://github.com/gakonst/ethers-rs/blob/ethers-v2.0.8/ethers-core/src/utils/mod.rs#L536
+/// Refer to the implementation for unit tests
+fn estimate_priority_fee(rewards: Vec<Vec<U256>>) -> U256 {
+    let mut rewards: Vec<U256> = rewards
+        .iter()
+        .map(|r| r[0])
+        .filter(|r| *r > U256::zero())
+        .collect();
+    if rewards.is_empty() {
+        return U256::zero();
+    }
+    if rewards.len() == 1 {
+        return rewards[0];
+    }
+    // Sort the rewards as we will eventually take the median.
+    rewards.sort();
+
+    // A copy of the same vector is created for convenience to calculate percentage change
+    // between subsequent fee values.
+    let mut rewards_copy = rewards.clone();
+    rewards_copy.rotate_left(1);
+
+    let mut percentage_change: Vec<I256> = rewards
+        .iter()
+        .zip(rewards_copy.iter())
+        .map(|(a, b)| {
+            let a = I256::try_from(*a).expect("priority fee overflow");
+            let b = I256::try_from(*b).expect("priority fee overflow");
+            ((b - a) * 100) / a
+        })
+        .collect();
+    percentage_change.pop();
+
+    // Fetch the max of the percentage change, and that element's index.
+    let max_change = percentage_change.iter().max().unwrap();
+    let max_change_index = percentage_change
+        .iter()
+        .position(|&c| c == *max_change)
+        .unwrap();
+
+    // If we encountered a big change in fees at a certain position, then consider only
+    // the values >= it.
+    let values = if *max_change >= ethers::utils::EIP1559_FEE_ESTIMATION_THRESHOLD_MAX_CHANGE.into()
+        && (max_change_index >= (rewards.len() / 2))
+    {
+        rewards[max_change_index..].to_vec()
+    } else {
+        rewards
+    };
+
+    // Return the median.
+    values[values.len() / 2]
 }
